9d1df2b1daa0721108e9755d91b8f81101761fa4
Junaid Babu <junaidvayoli@gmail.com>
2014-05-27 17:11:24 +0900

    timestamping is pain

--- app/database/migrations/2014_05_02_221041_create_users_table.php
+++ app/database/migrations/2014_05_02_221041_create_users_table.php
@@ -22,7 +22,9 @@ class CreateUsersTable extends Migration {
             $table->text('bio')->nullable();
             $table->integer('reputation')->default(0);
             $table->string('remember_token')->nullable();
-            $table->timestamps();
+            //$table->timestamps();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
         });
 	}
 
